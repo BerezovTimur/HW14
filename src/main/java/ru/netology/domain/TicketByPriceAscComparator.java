2610f12730f994bbf8b860965bbdb7a2b8058991
Timur Berezov <berezov.timur@gmail.com>
2020-06-09 16:21:27 +0300

    Change in TicketOffer

--- src/main/java/ru/netology/domain/TicketByPriceAscComparator.java
+++ src/main/java/ru/netology/domain/TicketByPriceAscComparator.java
@@ -7,8 +7,4 @@ public class TicketByPriceAscComparator implements Comparator<TicketOffer> {
     public int compare(TicketOffer o1, TicketOffer o2) {
         return o1.getTime() - o2.getTime();
     }
-
-    /*public int compare(TicketOffer o1, TicketOffer o2) {
-        return o1.getPrice() - o2.getPrice();
-    }*/
 }
